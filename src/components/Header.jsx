fd50b98377c082364b65af164e422900f69bd5aa
ahmed-kawsar <kawsar4698@gmail.com>
2022-12-08 09:35:11 +0600

    added meta to ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,6 +9,18 @@ const Header = ({ page }) => {
       <p className='tagline'>Fill all form field to go to next step</p>
       <IconBar page={page} />
       <ProgressBar page={page} />
+      <div className='meta'>
+        <h2>
+          {page === 0
+            ? 'Account Information'
+            : page === 1
+            ? 'Personal Information'
+            : page === 2
+            ? 'Image Upload'
+            : 'Finish'}
+        </h2>
+        <h2>{page + 1} / 4</h2>
+      </div>
     </header>
   )
 }
